f140ade41116b8c17d6f6dae5d77ea006254c279
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2025-04-30 11:49:15 +0200

    Add predict organoid script.

---
+++ source/predict_organoid.py
@@ -0,0 +1,42 @@
+import argparse
+from pathlib import Path
+
+import numpy as np
+import torch
+from pytorch_rdc_net.generic_model import RDCNet
+from tifffile import imread, imwrite
+
+
+def main(input_dir: Path, output_dir: Path, model_checkpoint: Path):
+    model = RDCNet.load_from_checkpoint(model_checkpoint)
+
+    input_files = input_dir.glob("*.tif")
+    for input_file in input_files:
+        img = imread(input_file).astype(np.float32)
+        if img.max() > 1:
+            img = np.clip(img / np.quantile(img, 0.999), 0, 1)
+
+        shape = img.shape
+        pad_z = (0, shape[0] % model.hparams.down_sampling_factor[0])
+        pad_y = (0, shape[1] % model.hparams.down_sampling_factor[1])
+        pad_x = (0, shape[2] % model.hparams.down_sampling_factor[2])
+        img = np.pad(img, (pad_z, pad_y, pad_x), mode="reflect")
+
+        pred = model.predict_instances(torch.from_numpy(img[np.newaxis, np.newaxis]))[0, 0, :shape[0], :shape[1], :shape[2]]
+
+        output_file = output_dir / (input_file.stem + "_SEG.tif")
+        imwrite(output_file, pred.astype(np.uint16))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_dir", type=Path, required=True, help="Input directory containing .tif files")
+    parser.add_argument("--output_dir", type=Path, required=True, help="Output directory for segmented .tif files")
+    parser.add_argument("--model_checkpoint", type=Path, required=True, help="Path to the model checkpoint")
+    args = parser.parse_args()
+
+    main(
+        Path(args.input_dir),
+        Path(args.output_dir),
+        Path(args.model_checkpoint)
+    )
---
+++ source/slurm-job_predict_Organoid.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH --job-name=RDCNET
+#SBATCH --cpus-per-task=8
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --output=slurm_logs/run-%j.out
+#SBATCH --error=slurm_logs/run-%j.err
+#SBATCH --partition=main
+#SBATCH --mem=48GB
+#SBATCH --gres=gpu:a10080g:1
+#SBATCH --time=8:00:00
+account="$1"
+working_dir="$2"
+input_dir="$3"
+output_dir="$4"
+model_checkpoint="$5"
+
+set -eu
+
+function display_memory_usage() {
+        set +eu
+        echo -n "[INFO] [$(date -Iseconds)] [$$] Max memory usage in bytes: "
+        cat /sys/fs/cgroup/memory/slurm/uid_$(id -u)/job_${SLURM_JOB_ID}/memory.max_usage_in_bytes
+        echo
+}
+
+trap display_memory_usage EXIT
+
+START=$(date +%s)
+STARTDATE=$(date -Iseconds)
+echo "[INFO] [$STARTDATE] [$$] Starting SLURM job $SLURM_JOB_ID"
+echo "[INFO] [$STARTDATE] [$$] Running in $(hostname -s)"
+echo "[INFO] [$STARTDATE] [$$] Working directory: $(pwd)"
+
+### PUT YOUR CODE IN THIS SECTION
+export SBATCH_ACCOUNT="$account"
+WD="$working_dir" pixi run predict_organoid --input_dir "$input_dir" --output_dir "$output_dir" --model_checkpoint "$model_checkpoint"
+
+### END OF PUT YOUR CODE IN THIS SECTION
+
+END=$(date +%s)
+ENDDATE=$(date -Iseconds)
+echo "[INFO] [$ENDDATE] [$$] Workflow execution time \(seconds\) : $(( $END-$START ))"
